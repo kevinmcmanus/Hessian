1ce5e52fa2e495968fd61f3ab970519ad47f9bfc
Kevin McManus <kevin.leo.mcmanus@gmail.com>
2023-05-20 15:46:19 -0600

    runge kutta

--- approximations.py
+++ approximations.py
@@ -16,6 +16,55 @@ def euler_method(dfx, x0, t):
         dt = t[i]-t[i-1]
         this_x = last_x + dx*dt
         last_x = out[i] = this_x
-    
-    return out
+    if nx == 1:
+        out = np.squeeze(out)
+    return out.T
         
+def runge_kutter2_method(dfx, x0, t):
+    x0a = np.atleast_1d(x0)
+    if x0a.ndim > 1:
+        raise ValueError('Only 1d array or scalar allowed')
+    
+    nx = x0a.shape[0]
+    nt = len(t)
+    out = np.full((nt, nx), np.nan)
+
+    last_x = out[0] = x0a
+
+    for i in range(1, nt):
+        dx1 = dfx(last_x) #how to handle optional t param?
+        h = t[i]- t[i-1]
+        dx2 = dfx(last_x+(h/2)*dx1)
+        this_x = last_x + h*dx2
+        last_x = out[i] = this_x
+
+    if nx == 1:
+        out = np.squeeze(out)
+    return out.T
+
+def runge_kutter4_method(dfx, x0, t):
+    x0a = np.atleast_1d(x0)
+    if x0a.ndim > 1:
+        raise ValueError('Only 1d array or scalar allowed')
+    
+    nx = x0a.shape[0]
+    nt = len(t)
+    out = np.full((nt, nx), np.nan)
+
+    last_x = out[0] = x0a
+
+    for i in range(1, nt):
+        h = t[i]- t[i-1]
+        dx1 = dfx(last_x) #how to handle optional t param?
+        dx2 = dfx(last_x+(h/2)*dx1)
+        dx3 = dfx(last_x+(h/2)*dx2)
+        dx4 = dfx(last_x+ h*dx3)
+
+        this_x = last_x + (h/6.)*(dx1 + 2.*dx2 + 2.*dx3 + dx4)
+
+        last_x = out[i] = this_x
+        
+    if nx == 1:
+        out = np.squeeze(out)
+    return out.T
+
--- testgrad.py
+++ testgrad.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 from gradient import gradient
-from approximations import euler_method
+from approximations import euler_method, runge_kutter2_method, runge_kutter4_method
+
+def rmse(y, yhat):
+    return np.sqrt(np.mean(np.power(y-yhat, 2)))
 
 def x2(x):
     return (x[0]**2+x[1]**2)
@@ -14,16 +18,53 @@ def d_expeq(y, param=0.3):
     return param*y
 
 gr = gradient(x2, np.array([0,400000]))
-
 print(f'gradient: {gr}')
 
-time_t = np.arange(0, 10, 0.5)
+time_t = np.arange(0, 30, 1.0)
 e_t = expeq(3, time_t)
+
 et_euler = euler_method(d_expeq, 3, time_t)
+rmse_euler = rmse(e_t, et_euler)
+
+et_rk2 = runge_kutter2_method(d_expeq, 3, time_t)
+rmse_rk2 = rmse(e_t, et_rk2)
+
+et_rk4 = runge_kutter4_method(d_expeq, 3, time_t)
+rmse_rk4 = rmse(e_t, et_rk4)
+
+print(f'euler shape: {et_euler.shape}')
 
 
 fig, ax = plt.subplots(figsize=(4,4))
-ax.plot(time_t, e_t, label='Exact')
-ax.plot(time_t, et_euler, label='Euler Approx')
-ax.legend()
+line1 = ax.plot(time_t[0], e_t[0], label='Exact')[0]
+line2 = ax.plot(time_t[0], et_euler[0], 
+                label=f'Euler Approx, RMSE: {rmse_euler:.2f}')[0]
+line3 = ax.plot(time_t[0], et_rk2[0],
+                 label=f'RK2 Approx, RMSE: {rmse_rk2:.2f}')[0]
+line4 = ax.plot(time_t[0], et_rk4[0],
+                 label=f'RK4 Approx, RMSE: {rmse_rk4:.2f}')[0]
+ax.set(xlim=[time_t.min(), time_t.max()],
+        ylim=[0, e_t.max()], xlabel='Time [s]', ylabel='Z [m]')
+ax.legend(loc='upper left')
+
+def update(frame):
+    # for each frame, update the data stored on each artist.
+    # x = time_t[:frame]
+    # et_exact= e_t[:frame]
+    # et_eu = et_euler[:frame]
+    # update the scatter plot:
+
+    # update the line plot:
+    line1.set_xdata(time_t[:frame])
+    line1.set_ydata(e_t[:frame])
+    line2.set_xdata(time_t[:frame])
+    line2.set_ydata(et_euler[:frame])
+    line3.set_xdata(time_t[:frame])
+    line3.set_ydata(et_rk2[:frame])
+    line4.set_xdata(time_t[:frame])
+    line4.set_ydata(et_rk4[:frame])
+    return (line1, line2,line3, line4)
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=len(time_t),
+                              interval=100, repeat=False)
 plt.show()
